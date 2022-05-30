d0ae6a6b07050cd3f0582f135183914703e51c3d
sophiecode1105 <sophiecode1105@gmail.com>
2022-05-30 18:21:57 +0900

    add: 컴포넌트 배치

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { TimeDisplayComponent } from './section/time-display/time-display.component';
+import { ButtonsComponent } from './section/buttons/buttons.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, SectionComponent, TimeDisplayComponent, ButtonsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
