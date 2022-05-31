5b3aa4c0a32b116dc021538b62a585d038eacf0a
sophiecode1105 <sophiecode1105@gmail.com>
2022-05-31 20:00:04 +0900

    style: 스타일 마무리

--- src/app/section/stopwatch/time-display/time-display.component.ts
+++ src/app/section/stopwatch/time-display/time-display.component.ts
@@ -22,9 +22,16 @@ export class TimeDisplayComponent implements OnInit {
   }
 
   timeStart() {
-    this.pageToggleService.plusCount();
     this.timeStop();
     this.timeInterval = setInterval(() => {
+      if (this.ms >= 100) {
+        this.ms = 0;
+        this.sec++;
+      }
+      if (this.sec >= 60) {
+        this.sec = 0;
+        this.min++;
+      }
       this.ms++;
     }, 10);
     clearInterval();
@@ -37,6 +44,8 @@ export class TimeDisplayComponent implements OnInit {
   timeReset() {
     this.timeStop();
     this.ms = 0;
+    this.sec = 0;
+    this.min = 0;
   }
 
   // ngOnChanges(changes: SimpleChanges) {
